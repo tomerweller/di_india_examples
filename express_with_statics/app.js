13368ba62d9e18e7cc913d0f3067ff12ecb03dab
Tomer Weller <tomer.weller@gmail.com>
2015-01-14 19:17:00 -0500

    init

---
+++ express_with_statics/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by tomerweller on 1/12/15.
+ */
+
+var express = require('express');
+var app = express();
+
+app.use("/", express.static(__dirname + "/static"));
+
+app.get("/", function (req, res) {
+    res.send('Hello Express!')
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port)
+});
